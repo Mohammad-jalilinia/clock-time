3b2b281eddf75aa9dd8161d0bac87eec12ac1d7b
Mohammad jalilinia <99993344+Mohammad-jalilinia@users.noreply.github.com>
2023-04-21 13:41:32 +0330

    Add files via upload

---
+++ clock_time.js
@@ -0,0 +1,34 @@
+let section = document.querySelector("section"),
+ icon = document.querySelector(".icons");
+//
+// icon.onclick = ()=>{
+//     section.classList.toggle("dark");
+// }
+
+function display(){
+    section.classList.toggle("dark");
+}
+// ************************************************
+
+setInterval(()=>{
+    let date = new Date()
+    hour = date.getHours()
+    min = date.getMinutes()
+    sec = date.getSeconds()
+
+    let d;
+    d = hour < 12 ? "AM" : "PM"
+    hour = hour >12 ? hour-12 :hour;
+    hour = hour == 0 ? hour=12 :hour;
+
+
+
+    document.querySelector(".hour-num").innerHTML = hour;
+    document.querySelector(".min-num").innerHTML = min;
+    document.querySelector(".sec-num").innerHTML = sec;
+    document.querySelector(".am-pm").innerHTML = d;
+
+
+
+
+},1000);
